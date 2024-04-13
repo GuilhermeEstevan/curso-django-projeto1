057dc5fab4d3412eb0025555f9b07df511c36c02
GuilhermeEstevan <guilhermeestevangui@hotmail.com>
2024-04-13 10:59:49 -0300

    static files

--- recipes/views.py
+++ recipes/views.py
@@ -5,12 +5,6 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "recipes/home.html", context={"name": "Guilherme Estevan"})
-
-
-def about(request):
-    return HttpResponse("Sobre")
-
-
-def contact(request):
-    return HttpResponse("Contato")
+    return render(
+        request, "recipes/pages/home.html", context={"name": "Guilherme Estevan"}
+    )
